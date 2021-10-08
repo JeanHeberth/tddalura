e05d5af6adb2cd82cf16a6f4850291db58391157
Jean Heberth <jeanheberth19@gmail.com>
2021-10-08 09:07:26 -0300

    Desempenho alterado

--- src/main/java/br/com/alura/tdd/modelo/Desempenho.java
+++ src/main/java/br/com/alura/tdd/modelo/Desempenho.java
@@ -3,32 +3,28 @@ package br.com.alura.tdd.modelo;
 import java.math.BigDecimal;
 
 public enum Desempenho {
-	A_DESEJAR {
-		@Override
-		public BigDecimal perecentualReajuste() {
+	   A_DESEJAR {
+	        @Override
+	        public BigDecimal perecentualReajuste() {
+
+
+	            return new BigDecimal("0.03");
+	        }
+	    }, Bom {
+	        @Override
+	        public BigDecimal perecentualReajuste() {
+
+	            return new BigDecimal("0.15");
+	        }
+	    }, Otimo {
+	        @Override
+	        public BigDecimal perecentualReajuste() {
+
+	            return new BigDecimal("0.20");
+	        }
+	    };
+
+	public abstract BigDecimal perecentualReajuste();
 		
-			
-			return null;
-		}
-	}, Bom {
-		@Override
-		public BigDecimal perecentualReajuste() {
-			
-			
-			return null;
-		}
-	}, Otimo {
-		@Override
-		public BigDecimal perecentualReajuste() {
-
-			
-			
-			return null;
-		}
-	};
-
 	
-	public abstract BigDecimal perecentualReajuste() {
-		
-	}
 }
